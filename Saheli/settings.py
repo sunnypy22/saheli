5b1899207f3426418d45a82037bc0b0093ee23d8
sunny-e-com <sunnypatel834798@gmail.com>
2021-07-19 22:18:29 +0530

    debug changes

--- Saheli/settings.py
+++ Saheli/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+1i*!incev5p8s2wym20b*#tj#cq-_#d#7i-i8dv-1xo^ft^b4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['https://saheli.herokuapp.com/',
