66827d986eb1b2304d0c11b80e64b891644eae36
sunny-e-com <sunnypatel834798@gmail.com>
2021-08-08 18:33:58 +0530

    two steps

--- Saheli/settings.py
+++ Saheli/settings.py
@@ -124,12 +124,13 @@ LOGIN_URL = 'signin'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'signin'
 
-EMAIL_USE_TLS = True
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "sunnypatel834798@gmail.com"
-EMAIL_HOST_PASSWORD = "sunny83479880"
+EMAIL_HOST_PASSWORD = "hsxqrtbklhcuieqq"
+EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "sunnypatel834798@gmail.com"
 
 # Static files (CSS, JavaScript, Images)
